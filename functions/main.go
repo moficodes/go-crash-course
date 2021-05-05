6069b7024d367bfa7ca7ae42b301cb624880f9a0
Mofizur Rahman <moficodes@gmail.com>
2021-05-04 21:35:42 -0400

    functions

--- functions/main.go
+++ functions/main.go
@@ -16,4 +16,58 @@ func init() {
 
 func main() {
 	fmt.Println("Hello World")
+	q, r := NamedDivide(5, 3)
+	fmt.Printf("Divide 5 / 3, Quotient: %d Remainder: %d\n", q, r)
+	fmt.Printf("Bill: %.2f\n", Bill(8.75, 10.2, 3.7))
+	Greet()
+
+	Greet := func() string {
+		return "shadow outer Greet"
+	}
+
+	TakeFunc(GiveFunc())
+	TakeFunc(Greet)
+}
+
+func Greet() {
+	fmt.Println("hello")
+}
+
+// this will fail
+// func Greet(m string) {
+// 	fmt.Println(m)
+// }
+
+func Add(a, b int) int {
+	return a + b
+}
+
+func Divide(a, b int) (int, int) {
+	return a / b, a % b
+}
+
+func NamedDivide(a, b int) (quotient int, remainder int) {
+	quotient, remainder = a/b, a%b
+	return
+}
+
+func TakeFunc(take func() string) {
+	data := take()
+	fmt.Println(data)
+}
+
+func GiveFunc() func() string {
+	f := func() string {
+		return "give"
+	}
+	return f
+}
+
+func Bill(tax float64, items ...float64) (total float64) {
+	for _, item := range items {
+		total += item
+	}
+
+	total *= (1 + tax*.01)
+	return
 }
--- importing/go.mod
+++ importing/go.mod
@@ -2,4 +2,4 @@ module importing
 
 go 1.16
 
-require github.com/rs/zerolog v1.21.0 // indirect
+require github.com/rs/zerolog v1.21.0
